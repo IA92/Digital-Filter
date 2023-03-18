530d84d9afd85ae1e0c507e97b9d66575e09e5b8
IA92 <30815233+IA92@users.noreply.github.com>
2023-03-18 13:11:43 +1100

    rename fft script

--- signal_fft.m
+++ signal_fft.m
@@ -5,10 +5,10 @@ fs = 50
 ts = 1.0/fs
 t = linspace(0, 1, fs)
 # Input signal
-freq = [1., 10., 15.]
+f_in = [1., 10., 15.]
 x = 0
 for i = 1: 3
-  x = x + sin(2*pi*freq(i)*t)
+  x = x + sin(2*pi*f_in(i)*t)
 end
 figure();
 plot(t,x);
@@ -18,12 +18,12 @@ title('Input data');
 N = length(x)
 n = 1:N
 T = N/fs
-freq = n/T
+f = n/T
 
 Y =fftn(x)
 
 figure()
-stem(freq,abs(Y))
+stem(f,abs(Y))
 xlabel('Freq (Hz)')
 ylabel('DFT Amplitude|X(freq)|')
 
